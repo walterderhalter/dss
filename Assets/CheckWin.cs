2a57d27fa810e5286d7721e24be4f2e407d0475b
jonathan.ohk <j.ohk@gebhardt-group.com>
2020-07-21 13:52:03 +0200

    Hey

---
+++ Assets/CheckWin.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CheckWin : MonoBehaviour
+{
+
+	public Sprite[] sprites;
+
+	// Start is called before the first frame update
+	void Start()
+    {
+		
+    }
+
+	public void check1()
+	{
+		Invoke("Check", 5);
+	}
+
+
+	public void Check()
+	{
+		int[] result = { 0, 0, 0 };
+
+		for (int i = 0; i < 3; i++)
+		{
+			result[i] = Random.Range(0, 2);
+			Debug.Log("Yeet"+i.ToString());
+			GameObject.Find("Image" + i.ToString()).GetComponent<Image>().sprite = sprites[result[i]];
+
+		}
+
+
+
+
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/SpriteRandomize.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SpriteRandomize : MonoBehaviour
+{
+	public Sprite[] sprites;
+	public float StopTime;
+	public int Erg;
+
+
+    void Start()
+    {
+        
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+		RandomImage();
+    }
+
+	 void RandomImage()
+	{
+		gameObject.GetComponent<UnityEngine.UI.Image>().sprite = sprites[UnityEngine.Random.Range(0, sprites.Length)];	
+	}
+
+	void EndRand()
+	{
+		enabled = false;
+		gameObject.GetComponent<UnityEngine.UI.Image>().sprite = null;
+	}
+
+	public void StopRand()
+	{
+		Invoke("EndRand",StopTime);
+	}
+
+	public void StartRand()
+	{
+		enabled = true;
+	}
+}
