00c30a5e39a3a4e0aeb9c6b29c202a8ff717bf66
jonathan.ohk <j.ohk@gebhardt-group.com>
2020-07-22 15:17:16 +0200

    Leiter Preis Logik

--- Assets/Leiter.cs
+++ Assets/Leiter.cs
@@ -7,25 +7,70 @@ using Random = UnityEngine.Random;
 
 public class Leiter : MonoBehaviour
 {
-	public int Money;
-	public int Win;
+
 
 
 	private int position;
 	private bool schalter;
 	private int c;
+	private List<int> price;
+	private protected int[] multipliaktoren = new int[] { 0, 2, 5, 7, 10, 20, 40, 70, 100, 200 };
 
 	// Start is called before the first frame update
 	void Start()
 	{
-		position = 4;
+		
+		SetPrice();
+		SetButtons();
+		position = GetPostion();
+
 		schalter = true;
 		GameObject.Find("box" + position).GetComponent<Image>().color = Color.green;
 	}
 
 
+	private void SetPrice()
+	{
+		price = new List<int>();
 
-	public void Drück()
+		for (int i = 0; i < 10; i++)
+		{
+			price[i] = multipliaktoren[i] * PlayerInfo.bet;
+		}
+
+	}
+	private void SetButtons()
+	{
+		for (int i = 0; i < 10; i++)
+		{
+			Debug.Log(i + 1);
+			GameObject.Find("txt" + (i+1)).GetComponent<Text>().text = price[i].ToString();
+		}
+	}
+
+	private int GetPostion()
+	{
+		if (price.Contains(PlayerInfo.winning))
+		{
+			return price.IndexOf(PlayerInfo.winning);
+		}
+
+		for (int i = 0; i < 10; i++)
+		{
+			if (PlayerInfo.winning > price[i] && PlayerInfo.winning < price[i])
+			{
+
+			}
+		}
+
+
+
+
+		return 4;
+	}
+
+
+	public void Start_click()
 	{
 		int rnd_int = Random.Range(0, 10);
 		GameObject.Find("box" + (position + 1)).GetComponent<Image>().color = Color.white;
@@ -40,6 +85,12 @@ public class Leiter : MonoBehaviour
 			position--;
 		}
 		GameObject.Find("box" + position).GetComponent<Image>().color = Color.green;
+
+	}
+
+	public void End_click()
+	{
+
 
 	}
 
