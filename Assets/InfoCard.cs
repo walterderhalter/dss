80e066187332f7cc2f105c1a52c962390e4962f5
jonathan.ohk <j.ohk@gebhardt-group.com>
2020-08-13 15:27:40 +0200

    Neues Scale System

---
+++ Assets/InfoCard.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InfoCard : MonoBehaviour
+{
+
+	public float speed = 2.0f;
+	private bool schalter = false;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		Debug.Log(transform.position);
+		enabled = false;
+		
+	}
+
+	public void StartClick()
+	{
+		enabled = true;
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+		if (schalter) {
+
+			if (transform.position.y > 7.4) { schalter = false; enabled = false; }
+
+			else
+				transform.position += transform.up * speed * Time.deltaTime;
+		}
+		else
+		{
+			if (transform.position.y < 2.9) { schalter = true; enabled = false; }
+
+			else
+				transform.position += -transform.up * speed * Time.deltaTime;
+		}
+	}
+}
